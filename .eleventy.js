a040883b5e053b4cf1be58a82e91bb46a7c5174d
Ethan Bliese <ethan@mirusresearch.com>
2023-07-25 13:14:44 -0500

    adding rss feed funcitionality

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,7 @@
-// const pluginRss = require('@11ty/eleventy-plugin-rss');
-
-
+const pluginRss = require('@11ty/eleventy-plugin-rss');
 
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addPlugin(pluginRss);
 	eleventyConfig.addPassthroughCopy("src/css");
 	eleventyConfig.addPassthroughCopy("src/images");
 	return {
